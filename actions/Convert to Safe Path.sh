573eddd2214e64c8a67de583836ae9ec72f1ff66
Evan Lovely <elovely@phase2technology.com>
2014-05-16 12:15:50 -0700

    Do not copy new line after. Fixes #1

--- actions/Convert to Safe Path.sh
+++ actions/Convert to Safe Path.sh
@@ -1,3 +1,3 @@
 # Replace spaces with underscores and make it all lowercase
-pbpaste | tr ' ' '_' | awk '{print tolower($0)}' | pbcopy
+pbpaste | tr ' ' '_' | awk '{print tolower($0)}' | tr -d '\n' | pbcopy
 echo "Spaces removed and lowercased in clipboard"
